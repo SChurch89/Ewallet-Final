d0e84e96800e0671ce895148f7d8a3b8d2d625be
MarkTHamilton <Hamilm25@ferris.edu>
2022-08-04 11:47:49 -0400

    EWallet
    "Creates list of incomes, expenses, and months. It also has the options to print income report by getting monthly information, print expense report for the specific month, print savings for the month. It displays each of the 3 and outputs the grand total by savings - expenses for the month = grand total. Final feature was the overall report which prints all 3.

---
+++ src/EWallet.java
@@ -0,0 +1,95 @@
+import java.util.ArrayList;
+
+public class EWallet {
+	private ArrayList<Month> months;                    //Declare income  ArrayList
+	private final String [] MONTHS = {"JANUARY", "FEBRUARY", "MARCH", "APRIL", "MAY", "JUNE", "JULY", "AUGUST", "SEPTEMBER", "OCTOBER", "NOVEMBER", "DECEMBER"};
+	
+	public EWallet() {
+		months = new ArrayList<Month>(12);              //Create List of incomes
+		for(int i = 0; i < 12; i++) {
+			months.add(new Month());
+			
+		}
+		
+	}
+	
+	public void addIncome(Income income, int month) {
+		months.get(month).addIncome(income);
+	}
+	
+	public void addExpense(Expense expense, int month) {
+		months.get(month).addExpense(expense);
+	}
+	public double totalIncome()  {
+		double total = 0;
+		for(int i = 0; i<months.size(); i++) {
+			total += months.get(i).monthlyIncome();
+		}
+		return total;
+	}
+	public double totalExpenses()  {
+		double total = 0;
+		for(int i = 0; i<months.size(); i++) {
+			total += months.get(i).monthlyExpenses();
+		}
+		return total;
+	}
+	public double totalSavings()  {
+		double total = 0;
+		for(int i = 0; i<months.size(); i++) {
+			total += months.get(i).monthlySavings();
+		}
+		return total;
+	}
+	public String printIncome()  {
+		String s="INCOME REPORT\n\n";
+		for(int i = 0; i<months.size(); i++) {
+			s+= MONTHS[i] + "\n";
+			s+= months.get(i).printMonthlyIncome()+"\n";
+		}
+		s+= "------------------------------\n";
+		s+= "GRAND TOTAL: $" + String.format("%.2f", totalIncome())+"\n";
+		return s;
+	}
+	public String printExpense()  {
+		String s="EXPENSES REPORT\n\n";
+		for(int i = 0; i<months.size(); i++) {
+			s+= MONTHS[i] + "\n";
+			s+= months.get(i).printMonthlyExpenses()+"\n";
+		}
+		s+= "------------------------------\n";
+		s+= "GRAND TOTAL: $" + String.format("%.2f", totalExpenses())+"\n";
+		return s;
+	}
+
+	public void addSaving(Income inc, int month) {
+		
+	}
+
+	public String printSavings() {
+		String s="SAVINGS REPORT\n\n";
+		for(int i = 0; i<months.size(); i++) {
+			s+= MONTHS[i] + "\n";
+			s+= months.get(i).printMonthlySavings()+"\n";
+		}
+		s+= "------------------------------\n";
+		s+= "GRAND TOTAL: $" + String.format("%.2f", totalSavings())+"\n";
+		return s;
+	}
+	public String printAll() {
+		String s="OVERALL REPORT\n\n";
+		s+= printIncome()+ "\n";
+		s+= printExpense()+ "\n";
+		s+= printSavings()+ "\n";
+		return s;
+	}
+}
+
+
+
+
+
+
+
+//EWallet list of months we have a way to add income
+//and expense to any given month 
